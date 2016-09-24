813e5e69d4f12f3567aff8b99885db6e059945da
kaileeagray <kaileeagray@gmail.com>
2016-09-24 12:40:59 -0500

    Add title helper, rails_defaults partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    base_title = "Cowork Fargo"
+    if base_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
 end
