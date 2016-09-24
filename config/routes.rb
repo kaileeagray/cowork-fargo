3df2325ddf3516981129c51c74405572520fe91d
kaileeagray <kaileeagray@gmail.com>
2016-09-24 18:09:43 -0500

    User view, user sign up, gravatar, static pages done ish. Start embedding map

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get '/about' => 'static_pages#about'
   get '/contact' => 'static_pages#contact'
 
+  get '/map' => 'maps#show'
+
   root 'static_pages#home'
 
   get '/signup', to: 'users#new'
