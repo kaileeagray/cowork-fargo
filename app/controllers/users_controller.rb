1a774757c32d2f85aae1259983f2320cd7aa15a0
kaileeagray <kaileeagray@gmail.com>
2016-09-24 17:32:12 -0500

    User sign up works with redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,23 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to Cowork Fargo!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email)
+    end
 
 end
