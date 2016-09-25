583044d09045565d422db3f39778d27b7294b43a
kaileeagray <kaileeagray@gmail.com>
2016-09-24 22:24:57 -0500

    Update js error so that public/assets can be pushed to heroku

--- app/assets/javascripts/map/gmap.js
+++
@@ -1,3 +0,0 @@
-<script src="//maps.google.com/maps/api/js?v=3.23&key=[your API key]"></script>
-<script src="//cdn.rawgit.com/mahnunchik/markerclustererplus/master/dist/markerclusterer.min.js"></script>
-<script src='//cdn.rawgit.com/printercu/google-maps-utility-library-v3-read-only/master/infobox/src/infobox_packed.js' type='text/javascript'></script> <!-- only if you need custom infoboxes -->
